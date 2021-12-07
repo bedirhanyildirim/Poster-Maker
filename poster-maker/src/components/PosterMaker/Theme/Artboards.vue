9adad6d9a12cccd1caad8854b22dd67aea2bb46d
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2021-12-07 17:19:51 +0300

    feat: Display artboards

--- poster-maker/src/components/PosterMaker/Theme/Artboards.vue
+++ poster-maker/src/components/PosterMaker/Theme/Artboards.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="artboards">
-    artboards
+    <div class="title">
+      <h2>Artboards</h2>
+    </div>
+    <div class="artboard">
+      Square (1:1)
+    </div>
+    <div class="artboard">
+      Instagram - Story (9:16)
+    </div>
   </div>
 </template>
 
@@ -15,8 +23,28 @@ export default {
   padding: 0;
   width: 302px;
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   background-color: #F7F7F7;
   border-left: 2px solid #E4E4E4;
+
+  .title h2{
+    font-size: 1rem;
+    color: #9F9F9F;
+    margin: 16px;
+    font-weight: normal;
+  }
+
+  .artboard {
+    margin: 8px 16px;
+    display: flex;
+    color: #505050;
+    font-size: .8rem;
+    text-align: left;
+    border-radius: 4px;
+    align-items: center;
+    background-color: #ffffff;
+    border: 1px solid #E4E4E4;
+    padding: 16px;
+  }
 }
 </style>
