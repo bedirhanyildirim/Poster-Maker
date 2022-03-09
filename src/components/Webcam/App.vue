f675f997b5e444ff56b6d992509728afc528a1b2
Bedirhan Yıldırım <bedirhanyildirim96@gmail.com>
2022-03-09 19:14:16 +0100

    feat: display camera on screen

--- src/components/Webcam/App.vue
+++ src/components/Webcam/App.vue
@@ -1,14 +1,41 @@
 <template>
 <div id="webcamApp">
-  Webcam App
+  <video autoplay id="videoElement"></video>
 </div>
 </template>
 
 <script>
 export default {
+  mounted() {
+    const video = document.querySelector("#videoElement")
+
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+          video.srcObject = stream
+        })
+        .catch(function (error) {
+          console.error(error)
+        })
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+#webcamApp {
+  margin: 0 auto;
+  width: fit-content;
+  border: 10px #333 solid;
 
+  #videoElement {
+    margin: 0;
+    padding: 0;
+    width: 960px;
+    height: 720px;
+    outline: none;
+    vertical-align: middle;
+    background-color: #666;
+  }
+}
 </style>
